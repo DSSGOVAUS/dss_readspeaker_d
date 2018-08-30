0875ca1bcc261514564ee89ab3752c6bfb2c90e1
Chris Lamb <chrislamb820@gmail.com>
2018-08-30 16:24:54 +1000

    Rewrite of DSS Readspeaker d8 module

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # DSS ReadSpeaker Block, Drupal 8 Version
 
-Adds a Readspeaker 'Listen' button as a customisable block. Suports https://
+Adds a ReadSpeaker 'Listen' button as a customisable block. Supports https://
 
 ##Dependencies
 * Block
-
-##TODO
-* Needs testing when (or if) we actually build a Drupal 8 site.
--- dss_readspeaker/src/Plugin/Block/ReadspeakerBlock.php
+++ dss_readspeaker/src/Plugin/Block/ReadspeakerBlock.php
@@ -16,7 +16,7 @@ class ReadspeakerBlock extends BlockBase {
   * {@inheritdoc}
   */
   public function build() {
-	
+
     $build = array();
     //Get Config vals
     $config = \Drupal::config('dss_readspeaker.credentials');
@@ -24,33 +24,23 @@ class ReadspeakerBlock extends BlockBase {
     $customerid = $config->get('customerid');
     $readid = $config->get('readid');
     $rslang = $config->get('rslang');
-    $region = $config->get('region').'.';
-    $rsstyles = $config->get('rsstyles');
-    $rscompact = $config->get('rscompact');
-    $rshttps = $config->get('rshttps');
+    $region = $config->get('region');
 
-	$proto = 'http';
-    if ($rshttps == true) {
-      $proto = 'https';
-      $region = ''; // region-code is removed for https requests
-    }
-	
-	$build['#attached']['library'][] = 'dss_readspeaker/dss-readspeaker';
+    // Add the library
+    $build['#attached']['library'][] = 'dss_readspeaker/dss-readspeaker';
 
-    // Custom styles
-    if ($rsstyles == true) {
-      $build['#attached']['library'][] = 'dss_readspeaker/dss-readspeaker.custom';
-      $output = '<div id="readspeaker_button" class="rs_skip rsbtn_colorskin rs_preserve">';
-      $output .= '<a class="rsbtn_play" href="'.$proto.'://app.'.$region.'readspeaker.com/cgi-bin/rsent?customerid='.$customerid.'&amp;lang='.$rslang.'&amp;readid='.$readid.'&amp;url='.$proto.'://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"].'">';
-      $output .= '<span class="rsbtn_left rspart rsimg"><span class="rsbtn_text"><span>Listen</span></span></span><span class="rsgrad"><span class="rsbtn_right rsplay rspart"></span></span></a></div>';
+    // Is it http or https?
+    $proto = 'http';
+    if (isset($_SERVER['HTTPS'])) {
+      if ($_SERVER['HTTPS'] != '') $proto = 'https';
     }
-    // Else, Default Readspeaker code
-    else {
-      $output = '<div id="readspeaker_button" class="rs_skip rsbtn rs_preserve">';
-      $output .= '<a class="rsbtn_play" href="'.$proto.'://app.'.$region.'readspeaker.com/cgi-bin/rsent?customerid='.$customerid.'&amp;lang='.$rslang.'&amp;readid='.$readid.'&amp;url='.$proto.'://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"].'">';
-      $output .= '<span class="rsbtn_left rspart rsimg"><span class="rsbtn_text"><span>Listen</span></span></span><span class="rsbtn_right rsimg rsplay rspart"></span></a></div>';
-    }
+
+    // Build Readspeaker code
+    $output = '<div id="readspeaker_button" class="rs_skip rsbtn rs_preserve">';
+    $output .= '<a rel="nofollow" class="rsbtn_play" href="'.$proto.'://app-'.$region.'.readspeaker.com/cgi-bin/rsent?customerid='.$customerid.'&amp;lang='.$rslang.'&amp;readid='.$readid.'&amp;url='.$proto.'://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"].'">';
+    $output .= '<span class="rsbtn_left rspart rsimg"><span class="rsbtn_text"><span>Listen</span></span></span><span class="rsbtn_right rsimg rsplay rspart"></span></a></div>';
+
     $build['#markup'] = $output;
-	return $build;
+	  return $build;
   }
 }
